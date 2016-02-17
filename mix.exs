14bb1c240d89f832b09b313ceae501029d46c3dc
Y <jsslzrd@gmail.com>
2016-02-17 22:48:41 +0100

    Bump deps

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule GuerillaRadio.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:slack, "~> 0.3.0"},
+     {:slack, "~> 0.4.2"},
      {:websocket_client, git: "https://github.com/jeremyong/websocket_client"}]
   end
 
